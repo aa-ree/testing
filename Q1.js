ca26a3c28a05242afa4883647c431780c63e132a
reehoiyan <re.hoiyan@gmail.com>
2016-08-17 13:55:53 +0800

    For question1, reverse the string.

---
+++ Q1.js
@@ -0,0 +1,16 @@
+function Str(s){
+	this.s = s;
+}
+
+Str.prototype.reverse = function(){
+	var s = this.s,
+		str = "";
+		
+		for(var i = s.length-1; i >= 0; i--){
+			str += s[i];
+		}
+		return str;
+}
+
+var s = new Str('hello');
+s.reverse();
