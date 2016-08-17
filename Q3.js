a2e79a64f28e7dd6c155b05210143d7dd8d6ac7d
reehoiyan <re.hoiyan@gmail.com>
2016-08-17 23:00:58 +0800

    refactor

--- Q3.js
+++ Q3.js
@@ -1,28 +1,29 @@
-function intervals(){
-  var arr1 = [[1,2],[3,5],[6,7],[8,10],[12,16]];
-  var merge = [4,9];
-  var max1, max2;
-  for(var i=0; i<arr1.length; i++){
-    //if(arr1[i][0] < merge[0] && arr1[i][1] > merge[0]){
-      //console.log('merge: '+merge[0]);
-    //}
-    if(arr1[i][1] > merge[0] && arr1[i][0] < merge[1]){
-      if(arr1[i][0] > merge[0] && arr1[i][1] < merge[1]){
-        var index = arr1.indexOf(arr1[i]);
-        arr1.splice(index,1);
-      }
-      if(arr1[i][0] < merge[1] && arr1[i][1] > merge[1]){
-        max1 = arr1[i][1];
-        var index = arr1.indexOf(arr1[i]);
-        arr1.splice(index,1);
-      }
-      if(arr1[i][1] < merge[1] && arr1[i][1] > merge[0] ){
-      max2 = merge[1];
-      arr1[i][1] = Math.max(max1,max2);
+function intervals(stack, merge){
+  var max = merge[1], min = merge[0];
+
+  //finding relation number
+  for(var i=0; i<stack.length; i++){
+    var overlap = stack[i][1] > merge[0] && stack[i][0] < merge[1];
+    if(overlap){
+        max = Math.max(stack[i][1], max);
+        min = Math.min(stack[i][0], min);
     }
-    }
-    
   }
- 
-  console.log(arr1);
+
+  //compare with origin num
+  for(var j=0; j<stack.length; j++){
+      var comp = [min, max];
+      if(stack[j][0] == min){
+        stack.splice(j,1,comp);
+      }
+      if(stack[j][0] > min && stack[j][1] <= max){
+        stack.splice(j--,1);
+      }
+  }
+  return stack;
 }
+
+console.log("Given intervals [1,3],[6,9], insert and merge [2,5] in as:");
+console.log(intervals([[1,3],[6,9]],[2,5]));
+console.log("Given [1,2],[3,5],[6,7],[8,10],[12,16], insert and merge [4,9] in as:");
+console.log(intervals([[1,2],[3,5],[6,7],[8,10],[12,16]], [4,9]));
